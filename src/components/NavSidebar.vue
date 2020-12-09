45c495b16859b6e7064dfd38ac924bbf62aa72ad
csiew <clarence.siew@gmail.com>
2020-12-10 01:09:54 +1100

    Style update for consistency with main website

--- src/components/NavSidebar.vue
+++ src/components/NavSidebar.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="z-index-200 position-fixed anchor-right width-auto noselect" v-bind:class="isVisible.sidebar === true ? (isMobile === true ? 'anchor-bottom width-full height-full' : 'anchor-top height-auto') : (isMobile === true ? 'anchor-bottom width-full height-auto' : 'anchor-top height-auto')">
-    <div class="vstack height-full align-end" v-bind:class="isMobile === true ? 'vstack-reverse' : ''">
-      <div class="nav-translucent z-index-200 hstack align-center padding-xs-top padding-xs-bottom" v-bind:class="isMobile === true ? 'nav nav-border-top justify-space-between width-full padding-s-left padding-s-right font-scale-xl' : 'card nav-shadow justify-end width-auto margin-s padding-s-left padding-s-right'">
+  <div id="navSidebarMenu" class="z-index-50 position-fixed anchor-right width-auto noselect" v-bind:class="isVisible.sidebar === true ? (isMobile === true ? 'anchor-bottom width-full height-full' : 'anchor-top height-auto margin-s-top margin-l-right') : (isMobile === true ? 'anchor-bottom width-full height-auto' : 'anchor-top height-auto margin-s-top margin-l-right')">
+    <div class="vstack height-full align-end" v-bind:class="isMobile === true ? 'vstack-reverse' : 'card nav-translucent'">
+      <div class="z-index-200 hstack align-center" v-bind:class="isMobile === true ? 'nav nav-border-top justify-space-between width-full font-scale-xl' : 'justify-end width-auto'">
         <transition name="fade">
-          <img v-show="helloIsVisible === false" class="grid-item profile-img profile-img-xs cursor-pointer nav-shadow border-radius-100pct margin-auto-vertical padding-none nodrag noselect" v-bind:class="isMobile === true ? 'margin-none-left margin-auto-right' : 'margin-s-right'" alt="profile" title="Back to top" src="@/assets/images/profile_0.jpg" v-on:click="this.goToSection('top'); isVisible.sidebar = false" />
+          <img v-show="helloIsVisible === false" class="grid-item profile-img profile-img-xs nav-shadow cursor-pointer border-radius-100pct margin-auto-vertical padding-none nodrag noselect" v-bind:class="isMobile === true ? 'margin-none-left margin-auto-right' : 'margin-s-left margin-xs-right'" alt="profile" title="Back to top" src="@/assets/images/profile_0.jpg" v-on:click="this.goToSection('top'); isVisible.sidebar = false" />
         </transition>
-        <button title="Toggle navigation menu" class="grid-item button-borderless cursor-pointer padding-xs line-height-1 font-scale-s" v-bind:class="isMobile === true ? 'margin-auto-left margin-none-right' : ''" v-on:click="isVisible.sidebar = !isVisible.sidebar">
+        <button title="Toggle navigation menu" class="grid-item button-borderless cursor-pointer padding-s padding-m-left padding-m-right line-height-1 font-scale-l" v-bind:class="isMobile === true ? 'margin-auto-left margin-none-right' : ''" v-on:click="isVisible.sidebar = !isVisible.sidebar">
           <span v-if="isVisible.sidebar === false">&#9776;</span>
           <span v-else>&#10005;</span>
         </button>
       </div>
       <transition v-bind:name="isMobile === true ? 'fade' : 'slide'">
-        <div v-if="isVisible.sidebar === true" class="card-list" v-bind:class="isMobile === true ? 'nav vstack align-center justify-center width-full height-full font-scale-l' : 'nav-translucent card width-auto margin-s-right'">
+        <div v-if="isVisible.sidebar === true" class="card-list" v-bind:class="isMobile === true ? 'nav vstack align-center justify-center width-full height-full font-scale-l' : 'nav-transparent nav-border-top width-auto'">
           <ul>
             <li v-for="item in this.sections" v-bind:key="item.id" v-show="!item.hide" class="cursor-pointer" v-bind:class="isMobile === true ? 'width-full padding-s hstack align-center justify-center' : 'padding-xl-left padding-m-right padding-xs-top padding-xs-bottom hstack align-end justify-center'" v-on:click="this.goToSection(item.id); isVisible.sidebar = false">
               {{ item.label }}
@@ -122,13 +122,14 @@ export default {
 </script>
 
 <style scoped>
+#navSidebarMenu {
+  box-shadow: var(--NAVBAR-SHADOW);
+  transition: var(--TRANSITION);
+}
+
 button {
   transform: scale(1) translateY(0%);
   transition: var(--TRANSITION);
-}
-button:hover {
-  color: var(--ACCENT-PRIMARY-COLOR) !important;
-  transform: scale(1.125) translateY(-5%);
 }
 button:active {
   transform: scale(0.96) translateY(0%);
@@ -158,10 +159,10 @@ button:active {
 .slide-enter-from,
 .slide-leave-to {
   opacity: 0;
-  transform: translate(50%, -75%) scale(0) skewX(-10deg);
+  transform: translateY(-50%) scale(0) skew(-25deg, 25deg);
 }
 .slide-enter-to {
   opacity: 1;
-  transform: translateY(0%, 0%) scale(1) skewX(0deg);
+  transform: translateY(0%) scale(1) skew(0deg, 0deg);
 }
 </style>
