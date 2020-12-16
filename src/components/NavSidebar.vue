d188ce0af10d1d04bf6fcb03c4ccfcf02bb48efc
csiew <clarence.siew@gmail.com>
2020-12-17 00:00:58 +1100

    Redesigned nav sidebar

--- src/components/NavSidebar.vue
+++ src/components/NavSidebar.vue
@@ -1,31 +1,27 @@
 <template>
-  <div id="navSidebarMenu" class="z-index-50 position-fixed anchor-right width-auto noselect" v-bind:class="isVisible.sidebar === true ? (isMobile === true ? 'anchor-bottom width-full height-full' : 'anchor-top height-auto margin-s-top margin-l-right') : (isMobile === true ? 'anchor-bottom width-full height-auto' : 'anchor-top height-auto margin-s-top margin-l-right')">
-    <div class="vstack height-full align-end" v-bind:class="isMobile === true ? 'vstack-reverse' : 'card nav-translucent'">
-      <div class="z-index-200 hstack align-center" v-bind:class="isMobile === true ? 'nav nav-border-top justify-space-between width-full font-scale-xl' : 'justify-end width-auto'">
-        <transition name="fade">
-          <img v-show="helloIsVisible === false" class="grid-item profile-img profile-img-xs nav-shadow cursor-pointer border-radius-100pct margin-auto-vertical padding-none nodrag noselect" v-bind:class="isMobile === true ? 'margin-s-left margin-auto-right' : 'margin-s-left margin-xs-right'" alt="profile" title="Back to top" src="@/assets/images/profile_0.jpg" v-on:click="this.goToSection('top'); isVisible.sidebar = false" />
-        </transition>
-        <button title="Toggle navigation menu" class="grid-item button-borderless cursor-pointer padding-s line-height-1 font-scale-l" v-bind:class="isMobile === true ? 'margin-auto-left' : ''" v-on:click="isVisible.sidebar = !isVisible.sidebar">
-          <span v-if="isVisible.sidebar === false">&#9776;</span>
-          <span v-else>&#10005;</span>
-        </button>
-      </div>
+  <div id="navSidebarMenu" class="vstack z-index-50 position-fixed anchor-left width-auto align-start noselect" v-bind:class="isVisible.sidebar === true ? 'height-full ' + (isMobile === true ? 'vstack-reverse anchor-bottom width-full' : 'nav-pill nav-pill-open nav-shadow nav-border-right anchor-top justify-space-between') : 'height-auto ' + (isMobile === true ? 'vstack-reverse anchor-bottom width-full' : 'nav-pill nav-shadow anchor-top')">
+    <div class="z-index-200 hstack align-center" v-bind:class="isMobile === true ? 'nav nav-border-top justify-space-between width-full font-scale-xl' : 'nav-pill-header justify-start width-auto padding-xxs'">
+      <button title="Toggle navigation menu" class="grid-item button-borderless cursor-pointer padding-s line-height-1 font-scale-l" v-on:click="isVisible.sidebar = !isVisible.sidebar">
+        <span v-if="isVisible.sidebar === false">&#9776;</span>
+        <span v-else>&#10005;</span>
+      </button>
       <transition name="fade">
-        <div v-if="isVisible.sidebar === true" v-bind:class="isMobile === true ? 'nav vstack align-center justify-center width-full height-full padding-none font-scale-l' : 'nav-transparent nav-border-top width-auto'">
-          <div class="card-list overflow-y-auto" v-bind:class="isMobile === true ? 'width-full height-full padding-m-top padding-m-bottom' : ''">
-            <ul v-bind:class="isMobile === true ? 'margin-auto-top margin-auto-bottom' : ''">
-              <li v-for="item in this.sections" v-bind:key="item.id" v-show="!item.hide" class="cursor-pointer" v-bind:class="isMobile === true ? 'width-full padding-xs hstack align-center justify-center font-scale-l' : 'padding-m-left padding-s-right padding-xs-top padding-xs-bottom hstack align-end justify-center'" v-on:click="this.goToSection(item.id); isVisible.sidebar = false">
-                {{ item.label }}
-              </li>
-            </ul>
-          </div>
-          <div v-if="isMobile === false" class="nav-border-top">
-            <div class="padding-s text-color-secondary" v-bind:class="isMobile === false ? 'width-max-240 margin-auto-left padding-xs-top padding-xs-bottom text-align-right font-scale-xs' : 'text-align-center font-scale-xxs'">
-              This portfolio site was built using Vue.js and is deployed on Netlify. It uses custom CSS utilities inspired by Bootstrap and Tailwind CSS.
-            </div>
-          </div>
-        </div>
+        <button v-show="helloIsVisible === false" class="grid-item button-hv-reveal cursor-pointer padding-xs line-height-1" v-bind:class="isMobile === true ? 'margin-s-right margin-auto-left font-scale-xxs' : 'margin-s-right margin-xs-left font-scale-s'" v-on:click="this.goToSection('top'); isVisible.sidebar = false">Back to top</button>
       </transition>
+    </div>
+    <div v-if="isVisible.sidebar === true" class="vstack height-full" v-bind:class="isMobile === true ? 'align-center justify-center width-full padding-none font-scale-l' : 'width-auto justify-space-between'">
+      <div class="card-list overflow-y-auto" v-bind:class="isMobile === true ? 'width-full height-full padding-m-top padding-m-bottom' : ''">
+        <ul v-bind:class="isMobile === true ? 'margin-auto-top margin-auto-bottom' : ''">
+          <li v-for="item in this.sections" v-bind:key="item.id" v-show="!item.hide" class="cursor-pointer" v-bind:class="isMobile === true ? 'width-full padding-xs hstack align-center justify-center font-scale-l' : 'padding-m-left padding-s-right padding-xs-top padding-xs-bottom hstack align-start justify-center'" v-on:click="this.goToSection(item.id); isVisible.sidebar = false">
+            {{ item.label }}
+          </li>
+        </ul>
+      </div>
+      <div v-if="isMobile === false" class="nav-border-top">
+        <div class="width-max-240 margin-auto-top padding-s text-align-left font-scale-xs justify-end text-color-secondary">
+          This portfolio site was built using Vue.js and is deployed on Netlify. It uses custom CSS utilities inspired by Bootstrap and Tailwind CSS.
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -130,8 +126,38 @@ export default {
 
 <style scoped>
 #navSidebarMenu {
-  box-shadow: var(--NAVBAR-SHADOW);
   transition: var(--TRANSITION);
+}
+
+.nav-pill {
+  background: var(--BG-COLOR);
+  border-bottom-right-radius: 16px !important;
+  animation: nav-pill-throbber 2s alternate-reverse infinite !important;
+}
+
+.nav-pill-header {
+  width: 100% !important;
+}
+.nav-pill-header,
+.nav-pill-header .button,
+.nav-pill-header button {
+  color: var(--BG-COLOR) !important;
+  background: var(--ACCENT-PRIMARY-COLOR) !important;
+  border-bottom-right-radius: 16px !important;
+}
+
+.nav-pill-open,
+.nav-pill-open .nav-pill-header {
+  border-bottom-right-radius: 0px !important;
+}
+
+@keyframes nav-pill-throbber {
+  from {
+    box-shadow: 0px 0px 8px var(--NAVBAR-SHADOW) !important;
+  }
+  to {
+    box-shadow: 0px 0px 16px var(--NAVBAR-SHADOW) !important;
+  }
 }
 
 button {
